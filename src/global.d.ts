dc93b13ed1e9c63aeb9dceaf33891dbf4b497b0c
sameer dhir <93440679+sameezy667@users.noreply.github.com>
2026-02-05 14:12:00 +0530

    fix: add process types to global.d.ts to resolve bbuild errors

--- src/global.d.ts
+++ src/global.d.ts
@@ -9,3 +9,16 @@ declare module '*.jpeg';
 declare module '*.gif';
 declare module '*.webp';
 declare module '*.ico';
+
+declare namespace NodeJS {
+    interface ProcessEnv {
+        readonly NODE_ENV: 'development' | 'production' | 'test';
+        readonly REACT_APP_API_URL: string;
+        readonly REACT_APP_OPENWEATHER_API_KEY: string;
+        [key: string]: string | undefined;
+    }
+}
+
+declare var process: {
+    env: NodeJS.ProcessEnv;
+};
